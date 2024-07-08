dae0b2ba87dfaeadc8411672fc6b2317c395b034
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-08 11:00:37 -0500

    Se elimino la carpeta 12_registros

---
+++ registro1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+struct emp{
+	int numero;
+	string nombre;
+	float ventas[12];
+	float salario;
+};
+int main(){
+	emp empleado[50];
+	int n;
+	cout<<"Ingrese el numero de empleados: ";cin>>n;
+	for(int i=0;i<n;i++){
+		cout<<"Ingrese el numero del empleado "<<i+1<<": ";cin>>empleado[i].numero;
+	    cout<<"Ingrese el nombre del empleado "<<i+1<<": ";cin.ignore();
+		getline(cin,empleado[i].nombre);
+		for(int j=0;j<12;j++){
+			cout<<"Ingrese la venta del mes "<<j+1<<":";cin>>empleado[i].ventas[j];
+		}cout<<"Ingrese el salario del empleado "<<i+1<<":";cin>>empleado[i].salario;
+	} 
+	return 0;
+	
+}
