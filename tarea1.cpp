403356701a0fac503f35f154eb0367ade1aca1f1
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-14 22:25:58 -0500

    Se agregó verificación del empleado con menor salario

--- tarea1.cpp
+++ tarea1.cpp
@@ -10,10 +10,10 @@ int main(){
 	emp empleado [n];
 	cout<<"Ingrese el numero de empleados: ";cin>>n;
 	for(int i=0;i<n;i++){
-	    cout<<"Ingrese el nombre del empleado: "<<i+1<<": ";cin.ignore();
+	    cout<<"Ingrese el nombre del empleado "<<i+1<<": ";cin.ignore();
 		getline(cin,empleado[i].nombre);
-		cout<<"Ingrese el sexo del empleado: "<<i+1<<": ";cin>>empleado[i].sexo;
-		cout<<"Ingrese el sueldo del empleado: "<<i+1<<":";cin>>empleado[i].sueldo;
+		cout<<"Ingrese el sexo del empleado "<<i+1<<": ";cin>>empleado[i].sexo;
+		cout<<"Ingrese el sueldo del empleado "<<i+1<<":";cin>>empleado[i].sueldo;
 	}
 	int mayor=0;
 	int menor=0;
@@ -21,10 +21,19 @@ int main(){
 		if(empleado[i].sueldo>mayor){
 			mayor=empleado[i].sueldo;
 			int masalario=i;
-			cout<<"El datos del empleado con mayor salario son los siguientes: "<<endl;
+			cout<<"Los datos del empleado con mayor salario son los siguientes: "<<endl;
 			cout<<"Nombre: "<<empleado[masalario].nombre<<endl;
 			cout<<"Sexo: "<<empleado[masalario].sexo<<endl;
 			cout<<"Salario: "<<empleado[masalario].sueldo<<endl;
 		}
+		if(empleado[i].sueldo<menor){
+			menor=empleado[i].sueldo;
+			int mesalario=i;
+			cout<<"Los datos del empleado con menor salario son los siguientes: "<<endl;
+			cout<<"Nombre: "<<empleado[mesalario].nombre<<endl;
+			cout<<"Sexo: "<<empleado[mesalario].sexo<<endl;
+			cout<<"Salario: "<<empleado[mesalario].sueldo<<endl;
+		}
 	}
+	return 0;
 }
