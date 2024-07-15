3951d47dfc85387bd1709c0dbed6eeff65468418
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-14 22:05:10 -0500

    Requerimiento del nombre, sexo y sueldo de los empleados

--- tarea1.cpp
+++ tarea1.cpp
@@ -5,3 +5,14 @@ struct empleado{
 	string sexo;
 	int sueldo;
 };
+int main(){
+	empleado DataTech [999];
+	int n;
+	cout<<"Ingrese el numero de empleados: ";cin>>n;
+	for(int i=0;i<n;i++){
+	    cout<<"Ingrese el nombre del empleado "<<i+1<<": ";cin.ignore();
+		getline(cin,empleado[i].nombre);
+		cout<<"Ingrese el sexo del empleado "<<i+1<<": ";cin>>empleado[i].sexo;
+		cout<<"Ingrese el sueldo del empleado "<<i+1<<":";cin>>empleado[i].sueldo;
+	}
+}
