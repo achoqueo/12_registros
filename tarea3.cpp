70d3e2144a843f77823a239c73085456b5c38d62
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-14 23:34:16 -0500

    Se agregó verificación para comprar el mes

--- tarea3.cpp
+++ tarea3.cpp
@@ -17,4 +17,17 @@ int main(){
 		cout<<"Ingrese el mes de nacimiento de la persona "<<i+1<<":";cin>>persona[i].mes;
 		cout<<"Ingrese el ano de nacimiento de la persona "<<i+1<<":";cin>>persona[i].ano;
 	}
+	int m;
+	do{
+		cout<<"Ingrese el numero del mes a comparar: ",cin>>m;
+		cout<<"Los datos de las personas que cumplen anos durante ese mes son los siguientes: "<<endl;
+		for(int i=0;i<n;i++){
+		if(m==persona[i].mes){
+		  cout<<"Nombre: "<<persona[i].nombre<<endl;
+		  cout<<"Dia de nacimiento: "<<persona[i].dia<<endl;
+		  cout<<"Ano de nacimiento: "<<persona[i].ano<<endl;	
+		}
+		}
+	} while(m!=0);
+	return 0;
 }
