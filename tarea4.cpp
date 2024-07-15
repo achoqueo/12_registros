5f57bee4d2cc809d84b087196da27d5bbe72d3ef
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-14 23:38:49 -0500

    Se almacenan los datos

--- tarea4.cpp
+++ tarea4.cpp
@@ -6,3 +6,15 @@ struct datos{
 	string disciplina;
 	int medallas;
 };
+int main(){
+	int n;
+	cout<<"Ingrese el numero de atletas: ";cin>>n;
+	datos atleta [n];
+	for(int i=0;i<n;i++){
+	    cout<<"Ingrese el nombre del empleado "<<i+1<<": ";cin.ignore();
+		getline(cin,atleta[i].nombre);
+		cout<<"Ingrese el pais del atleta "<<i+1<<": ";cin>>atleta[i].pais;
+		cout<<"Ingrese la disciplina del atleta "<<i+1<<":";cin>>atleta[i].disciplina;
+		cout<<"Ingrese el numero de medallas del atleta "<<i+1<<":";cin>>atleta[i].medallas;
+	}
+}
