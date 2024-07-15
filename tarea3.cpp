04d6354e930276ef3397b100d08c33004e1dd571
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-14 22:59:52 -0500

    Se almacenan los datos de las personas, nombre, dia, mes y año de nacimiento

--- tarea3.cpp
+++ tarea3.cpp
@@ -6,3 +6,15 @@ struct datos{
 	int mes;
 	int ano;
 };
+int main(){
+	int n;
+	cout<<"Ingrese el numero de personas: ";cin>>n;
+	datos persona [n];
+	for(int i=0;i<n;i++){
+	    cout<<"Ingrese el nombre de la persona "<<i+1<<": ";cin.ignore();
+		getline(cin,persona[i].nombre);
+		cout<<"Ingrese el dia de nacimiento de la persona "<<i+1<<": ";cin>>persona[i].dia;
+		cout<<"Ingrese el mes de nacimiento de la persona "<<i+1<<":";cin>>persona[i].mes;
+		cout<<"Ingrese el ano de nacimiento de la persona "<<i+1<<":";cin>>persona[i].ano;
+	}
+}
