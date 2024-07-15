34ae953775f8a6f274087b962ab1277386f8dc27
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-14 22:32:17 -0500

    almacenamiento de la información

--- tarea2.cpp
+++ tarea2.cpp
@@ -5,3 +5,14 @@ struct per{
 	int dni;
 	int edad;
 };
+int main(){
+	int n;
+	per persona [n];
+	cout<<"Ingrese el numero de personas: ";cin>>n;
+	for(int i=0;i<n;i++){
+	    cout<<"Ingrese el nombre de la persona "<<i+1<<": ";cin.ignore();
+		getline(cin,persona[i].nombre);
+		cout<<"Ingrese el numero de DNI de la persona "<<i+1<<": ";cin>>persona[i].dni;
+		cout<<"Ingrese la edad de la persona "<<i+1<<":";cin>>persona[i].edad;
+	}
+}
