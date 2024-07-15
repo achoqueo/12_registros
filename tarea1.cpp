bb7c7036eec97893d5f319d83b12fe1adfdf117b
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-14 22:22:02 -0500

    Corrección de la estructura y se agregó verificación del empleado con mayor salario

--- tarea1.cpp
+++ tarea1.cpp
@@ -1,18 +1,30 @@
 #include<iostream>
 using namespace std;
-struct empleado{
+struct emp{
 	string nombre;
 	string sexo;
 	int sueldo;
 };
 int main(){
-	empleado DataTech [999];
 	int n;
+	emp empleado [n];
 	cout<<"Ingrese el numero de empleados: ";cin>>n;
 	for(int i=0;i<n;i++){
-	    cout<<"Ingrese el nombre del empleado "<<i+1<<": ";cin.ignore();
+	    cout<<"Ingrese el nombre del empleado: "<<i+1<<": ";cin.ignore();
 		getline(cin,empleado[i].nombre);
-		cout<<"Ingrese el sexo del empleado "<<i+1<<": ";cin>>empleado[i].sexo;
-		cout<<"Ingrese el sueldo del empleado "<<i+1<<":";cin>>empleado[i].sueldo;
+		cout<<"Ingrese el sexo del empleado: "<<i+1<<": ";cin>>empleado[i].sexo;
+		cout<<"Ingrese el sueldo del empleado: "<<i+1<<":";cin>>empleado[i].sueldo;
+	}
+	int mayor=0;
+	int menor=0;
+	for(int i=0;i<n;i++){
+		if(empleado[i].sueldo>mayor){
+			mayor=empleado[i].sueldo;
+			int masalario=i;
+			cout<<"El datos del empleado con mayor salario son los siguientes: "<<endl;
+			cout<<"Nombre: "<<empleado[masalario].nombre<<endl;
+			cout<<"Sexo: "<<empleado[masalario].sexo<<endl;
+			cout<<"Salario: "<<empleado[masalario].sueldo<<endl;
+		}
 	}
 }
