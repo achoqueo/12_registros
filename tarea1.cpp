3a8a92ff6fa2d6e324d5e4a6483ab5ae34d95ce3
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-14 23:11:36 -0500

    Mejoramiento del código

--- tarea1.cpp
+++ tarea1.cpp
@@ -15,25 +15,30 @@ int main(){
 		cout<<"Ingrese el sexo del empleado "<<i+1<<": ";cin>>empleado[i].sexo;
 		cout<<"Ingrese el sueldo del empleado "<<i+1<<":";cin>>empleado[i].sueldo;
 	}
-	int mayor=0;
-	int menor=0;
-	for(int i=0;i<n;i++){
-		if(empleado[i].sueldo>mayor){
-			mayor=empleado[i].sueldo;
-			int masalario=i;
-			cout<<"Los datos del empleado con mayor salario son los siguientes: "<<endl;
-			cout<<"Nombre: "<<empleado[masalario].nombre<<endl;
-			cout<<"Sexo: "<<empleado[masalario].sexo<<endl;
-			cout<<"Salario: "<<empleado[masalario].sueldo<<endl;
-		}
-		if(empleado[i].sueldo<menor){
-			menor=empleado[i].sueldo;
-			int mesalario=i;
-			cout<<"Los datos del empleado con menor salario son los siguientes: "<<endl;
-			cout<<"Nombre: "<<empleado[mesalario].nombre<<endl;
-			cout<<"Sexo: "<<empleado[mesalario].sexo<<endl;
-			cout<<"Salario: "<<empleado[mesalario].sueldo<<endl;
-		}
-	}
+	int mayor = empleado[0].sueldo;
+    int masalario = 0;
+    for(int i=0; i<n;i++) {
+        if(empleado[i].sueldo>mayor) {
+            mayor = empleado[i].sueldo;
+            masalario = i;
+        }
+    }
+    cout << "Los datos del empleado con mayor salario son los siguientes: " << endl;
+    cout << "Nombre: " << empleado[masalario].nombre << endl;
+    cout << "Sexo: " << empleado[masalario].sexo << endl;
+    cout << "Salario: " << empleado[masalario].sueldo << endl;
+
+    int menor = empleado[0].sueldo;
+    int mesalario = 0;
+    for(int i=0;i<n;i++) {
+        if(empleado[i].sueldo<menor) {
+            menor = empleado[i].sueldo;
+            mesalario = i;
+        }
+    }
+    cout << "Los datos del empleado con menor salario son los siguientes: " << endl;
+    cout << "Nombre: " << empleado[mesalario].nombre << endl;
+    cout << "Sexo: " << empleado[mesalario].sexo << endl;
+    cout << "Salario: " << empleado[mesalario].sueldo << endl;
 	return 0;
 }
